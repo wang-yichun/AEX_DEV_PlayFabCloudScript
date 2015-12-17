cf5ed79676351b47f6ab768d6b130b3e2379c3dc
Ethan <yishionwang314@163.com>
2015-12-17 11:53:18 +0800

    update

---
+++ cloudscript.js
@@ -0,0 +1,76 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// PogoRockGame - AEX 
+// CloudScript
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+handlers.LikeRecord = function (args) {
+
+    log.info("== LikeRecord Began");
+
+    var like_key = args.StageId + "_user_rec_like"; 
+
+    var result = server.GetUserData({
+        PlayFabId: args.PlayFabId,
+        Keys: [like_key]
+    });
+
+    log.info(result);
+    log.info("== LikeRecord End");
+}
+
+// If a user login for the first time, many operations will be done at the server.
+// Especially to initialize user data.
+// 初始化一个用户的云端数据,用户在第一次登陆后需要调用(或:发现用户只读数据的need_init==1时调用)
+handlers.PrepareForNewUser = function (args) {
+    
+    // server.GrantItemsToUser({
+    //     CatalogVersion : "Alpha",
+    //     PlayFabId: currentPlayerId,
+    //     Annotation: "Test",
+    //     ItemIds: ["GA", "GB", "GC"]
+    // });
+    
+    server.UpdateUserData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            LineLevel: 1,
+            TotalExp: 0,
+            TotalStar: 0,
+            CurrentStageID: "level_001"
+        },
+        Permission: "Public"
+    });
+    
+    server.UpdateUserData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            GA1: 0,
+            GA2: 0,
+            GA3: 0,
+            GAPower: 0,
+            GACreateStartTime: "",
+            GB1: 0,
+            GB2: 0,
+            GB3: 0,
+            GBPower: 0,
+            GBCreateStartTime: "",
+            GC1: 0,
+            GC2: 0,
+            GC3: 0,
+            GCPower: 0,
+            GCCreateStartTime: "",
+            GX: 0,
+            StageInfoDataVersion: 0
+        },
+        Permission: "Private"
+    });
+    
+    server.UpdateUserReadOnlyData({
+        PlayFabId: currentPlayerId,
+        Data: {
+            need_init: 0
+        }
+    });
+}
