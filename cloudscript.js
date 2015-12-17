2a658dd929c5eb4f1f5a56790874d56045fda96e
Ethan <yishionwang314@163.com>
2015-12-17 16:56:21 +0800

    update

--- cloudscript.js
+++ cloudscript.js
@@ -7,17 +7,41 @@
 // https://github.com/wang-yichun/AEX_PlayFabCloudScript.git
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// 点赞
+// return: {statis = 0:正确 1:没有发现录像 2:已经点过赞了}
 handlers.LikeRecord = function (args) {
 
     var play_fab_id = args.PlayFabId;
     var stage_id = args.StageId;
 
-    var like_key = stage_id + "_user_rec_like"; 
+    var like_key = stage_id + "_user_rec_like";
+    var s3_key = stage_id + "_user_rec_data_s3_key";
 
     var user_data = server.GetUserData({
         PlayFabId: play_fab_id,
-        Keys: [like_key]
+        Keys: [like_key, s3_key]
     });
+
+    var s3_value_data = user_data.Data[s3_key];
+    var s3_value;
+    
+    // 处理没有s3的key的情况
+    if（s3_value_data == undefined) {
+        return {status: 1};
+    } else {
+        s3_value = s3_value_data.Value;
+    }
+
+    // 检查自己是否已经点过赞了
+    var internal_like_rec_key = "like_rec_" + s3_value;
+    var user_data_2 = server.GetUserData({
+        PlayFabId: currentPlayerId,
+        Keys: [internal_like_rec_key]
+    });
+    var already_like_data = user_data_2.Data[internal_like_rec_key];
+    if (already_like_data != null && already_like_data.Value == 1) {
+        return {status: 2};
+    }
 
     var like_value_data = user_data.Data[like_key];
     var like_value = undefined;
@@ -29,18 +53,28 @@ handlers.LikeRecord = function (args) {
         like_value = like_value_data.Value;
     }
 
+    // 更新计数
     like_value++;
 
-    var data = {};
-    data[like_key] = like_value;
+    var data_1 = {};
+    data_1[like_key] = like_value;
 
     server.UpdateUserData({
         PlayFabId: play_fab_id,
-        Data: data,
+        Data: data_1,
         Permission: "public"
     });
     
+    // 添加internal key，对应点赞某录像
+    var data_2 = {};
+    data_2[internal_like_rec_key] = 1;
+    server.UpdateUserInternalData({
+        PlayFabId: currentPlayerId,
+        Data: data_2
+    });
+
     log.info("set " + like_key + " = " + like_value);
+    return {status: 0};
 }
 
 // If a user login for the first time, many operations will be done at the server.
