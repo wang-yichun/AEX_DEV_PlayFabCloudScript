043c49c461bb50900643666c0224973af522e11c
Ethan <yishionwang314@163.com>
2016-02-26 18:53:54 +0800

    update

--- Friend_SendHeart.js
+++ Friend_SendHeart.js
@@ -4,11 +4,25 @@
 //
 
 handlers.SendHeart = function(args) {
+
+    var heart_send_from_key = "heart_send_from";
     var sender_id = args.sender_id;
     var receiver_ids = args.receiver_ids;
 
-    log.info("sender_id: " + sender_id);
-    log.info("receiver_ids: " + receiver_ids);
+    // log.info("sender_id: " + sender_id);
+    // log.info("receiver_ids: " + receiver_ids);
+
+    for (var i = 0; i < receiver_ids.length; i++) {
+        var receiver_id = receiver_ids[i];
+        var heart_send_from_result = server.GetUserData({
+            PlayFabId: receiver_id,
+            Keys: [heart_send_from_key]
+        });
+        var heart_send_from = heart_send_from_result.Data[heart_send_from_key];
+
+        log.info("i: " + i + "heart_send_from: " + heart_send_from);
+    };
+
 
     return { status: 0 };
 }
