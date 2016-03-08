3dd8aa1ab092191fb7545a0f0e52cfb3145ed671
Ethan <yishionwang314@163.com>
2016-03-08 13:58:25 +0800

    update

--- Common_GetLineLevelByIDs.js
+++ Common_GetLineLevelByIDs.js
@@ -1,26 +1,28 @@
 /////////////////////////////////////////////////////////
-// 扩展获取排行榜
+// 批量获取线长
 // [{"PlayFabId":"AA25328A8D0031FA","DisplayName":"painache","StatValue":1355500,"Position":0,"LineLevel":1},
 //  {"PlayFabId":"9241B8F15709F6C6","DisplayName":"feng_bcdeaa42","StatValue":1355100,"Position":1,"LineLevel":1}]
 
-handlers.GetStatisticsByIDs = function(args) {
+handlers.GetLineLevelByIDs = function(args) {
 
     var level_id = args.level_id;
     var id_list = args.id_list;
 
-    var score_key = level_id + "_user_stat_score";
+    // var score_key = level_id + "_user_stat_score";
     var stage_info_key = level_id + "_user_stat_info_list";
 
     var response_list = [];
 
     for (var i = 0; i < id_list.length; i++) {
         var playfab_id = id_list[i];
-        var result = server.GetPlayerStatistics({
+        var result = server.GetUserData({
             PlayFabId: playfab_id,
-            StatisticNames: [
-                score_key
+            Keys: [
+                stage_info_key
             ]
         });
+
+        
 
         response_list.push(result);
     }
