9cbb5b64de75716d73975d9c863267a3cab046fd
Ethan <yishionwang314@163.com>
2016-11-01 19:30:11 +0800

    upd

--- Notification_SendPushHearts.js
+++ Notification_SendPushHearts.js
@@ -6,10 +6,26 @@ handlers.SendPushHearts = function (args)
 	var playfab_ids = args.PlayFabIds;
 	for (var i = 0; i < playfab_ids.length; i++){
 		var playfab_id = playfab_ids[i];
-		
-		var msg_text = handlers.GetPlayerReceiveHeartMessage({
-        	PlayFabId: playfab_id
-    	}).language;
+
+		var result = server.GetUserData({
+	        PlayFabId: playfab_id,
+	        Keys: ["language"]
+	    });
+	    
+	    var language = result.Data["language"];
+	    var msg_text = "Received a heart from your friend!";
+
+	    if (language == null || language.Value == ""){
+	        log.info("language is null");
+	    } else {
+	        if (language.Value == "German") {
+	            msg_text = "dein freund hat ein herz für sie!";
+	        } else if (language.Value == "Chinese") {
+	            msg_text = "你的好友送给你了一颗心!";
+	        } else if (language.Value =="Chinese (Hong Kong)"){
+	            msg_text = "你的朋友送來了一顆心!";
+	        }
+	    }
 
 		var request = {};
 		request.Recipient = playfab_id;
