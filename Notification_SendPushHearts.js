dd454bdf378ff997dcfaafc3549877fc5194c2aa
Ethan <yishionwang314@163.com>
2016-11-30 16:00:43 +0800

    upd

--- Notification_SendPushHearts.js
+++ Notification_SendPushHearts.js
@@ -7,26 +7,26 @@ handlers.SendPushHearts = function (args)
 	for (var i = 0; i < playfab_ids.length; i++){
 		var playfab_id = playfab_ids[i];
 
-		// var result = server.GetUserData({
-	 //        PlayFabId: playfab_id,
-	 //        Keys: ["language"]
-	 //    });
+		var result = server.GetUserData({
+	        PlayFabId: playfab_id,
+	        Keys: ["language"]
+	    });
 	    
-	 //    var language = result.Data["language"];
+	    var language = result.Data["language"];
 	    var msg_text = "Received a heart from your friend!";
 
-	    // if (language == null || language.Value == ""){
-	    //     log.info("language is null");
-	    // } else {
-	    // 	log.info("language is " + language.Value);
-	    //     if (language.Value == "German") {
-	    //         msg_text = "dein freund hat ein herz für sie!";
-	    //     } else if (language.Value == "Chinese") {
-	    //         msg_text = "你的好友送给你了一颗心!";
-	    //     } else if (language.Value =="Chinese (Hong Kong)"){
-	    //         msg_text = "你的朋友送來了一顆心!";
-	    //     }
-	    // }
+	    if (language == null || language.Value == ""){
+	        log.info("language is null");
+	    } else {
+	    	log.info("language is " + language.Value);
+	        if (language.Value == "German") {
+	            msg_text = "dein freund hat ein herz für sie!";
+	        } else if (language.Value == "Chinese") {
+	            msg_text = "你的好友送给你了一颗心!";
+	        } else if (language.Value =="Chinese (Hong Kong)"){
+	            msg_text = "你的朋友送來了一顆心!";
+	        }
+	    }
 
 		var spn_req = {};
 		spn_req.Recipient = playfab_id;
