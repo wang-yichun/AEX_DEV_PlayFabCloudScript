c107d8d87aa6c7c0d1b6396e4b263da6b6868015
Ethan <yishionwang314@163.com>
2017-01-17 15:32:43 +0800

    upd gather user login

--- GatherUserLoginInfo.js
+++ GatherUserLoginInfo.js
@@ -6,14 +6,25 @@ handlers.GatherUserLoginInfo = function (args, context) {
     // The event that triggered the action 
     // (https://api.playfab.com/playstream/docs/PlayStreamEventModels)
     var psEvent = context.playStreamEvent;
-    
+    var location = psEvent.TriggeringEventData.Location;
+
+    var locationJson = JSON.stringify(location);
+
+    server.UpdateUserData({
+		PlayFabId: currentPlayerId,
+		Data: {
+			location_json: locationJson
+		},
+		Permission: "Public"
+	});
+
     // The profile data of the player associated with the event
     // (https://api.playfab.com/playstream/docs/PlayStreamProfileModels)
-    var profile = context.playerProfile;
+    // var profile = context.playerProfile;
     
     // Post data about the event to an external API
-    var content = JSON.stringify({user: profile.PlayerId, event: psEvent.EventName});
-    var response = http.request('https://httpbin.org/status/200', 'post', content, 'application/json', null, true);
+    // var content = JSON.stringify({user: profile.PlayerId, event: psEvent.EventName});
+    // var response = http.request('https://httpbin.org/status/200', 'post', content, 'application/json', null, true);
     
-    return { externalAPIResponse: response };
+    return { result: 0 };
 }
