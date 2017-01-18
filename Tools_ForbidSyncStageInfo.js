e6ac1274a08475df5e04df416836b65218f7c15b
Ethan <yishionwang314@163.com>
2017-01-18 16:46:10 +0800

    upd

--- Tools_ForbidSyncStageInfo.js
+++ Tools_ForbidSyncStageInfo.js
@@ -1,11 +1,11 @@
 ////////////////////////////////////////////////
 // 阻止用户上传成绩到排行榜, 针对测试版本的老用户
 
-handlers.ForbidSyncStageInfo = function (args) {
+handlers.ForbidSyncStageInfo = function (args, context) {
 	// 获取用户版本:
 	var version_key = "version";
 	var user_data = server.GetUserData({
-		PlayFabId: play_fab_id,
+		PlayFabId: currentPlayerId,
 		Keys: [version_key]
 	});
 	var version_data = user_data.Data[version_key];
@@ -16,6 +16,20 @@ handlers.ForbidSyncStageInfo = function (args) {
 		version_value = version_data.Value;
 	}
 	log.info("version_value: " + version_value);
+
+	var title_misctable_key = "MiscTable";
+	var title_data = server.GetTitleData({
+		Keys: [title_misctable_key]
+	});
+	var min_version_data = title_data.Data[title_misctable_key];
+	var min_version_value;
+	if (min_version_data == null){
+		return {status: 2}
+	} else {
+		log.info("min_version_data: " + JSON.stringify(min_version_data);
+		min_version_value = min_version_data.Value;
+	}
+	log.info("min_version_value: " + min_version_value);
 
 	return {status: 0};
 }
